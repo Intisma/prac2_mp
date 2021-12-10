bfd95f30b64d1119769903d75bbff3199962dd0c
alexiavp <alexia.vidal.pons@gmail.com>
2021-12-10 22:25:12 +0100

    Funciones read and write from file

--- src/inputOutput/WriteData.java
+++ src/inputOutput/WriteData.java
@@ -1,4 +1,32 @@
 package inputOutput;
 
+import information.ADTsetResources;
+
+import java.io.*;
+
 public class WriteData {
+
+    /**
+     * Empty constructor
+     */
+    public WriteData() {
+    }
+
+    /**
+     * Write the data in a result file
+     * @param information to save in the file
+     * @param file name of the file
+     * @return boolean to know if the writing gone correctly
+     */
+    public boolean write(ADTsetResources information, String file) {
+        try {
+            BufferedWriter result = new BufferedWriter(new FileWriter(file));
+            result.write(information.toString());
+            result.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
 }
