12e646529e6c2b7680df07a6b8cbfd3290195553
Ismael <ismaelprsa00@gmail.com>
2021-12-12 18:39:16 +0100

    Generated data

--- src/timeMeasurer/TimeExecute.java
+++ src/timeMeasurer/TimeExecute.java
@@ -4,7 +4,7 @@ import inputOutput.WriteTimes;
 
 public class TimeExecute {
     public static void main(String[] args) {
-        Times times = TimeMeasurer.sizeEvolution(50000, 10, false);
+        Times times = TimeMeasurer.sizeEvolution(100000, 10, false);
         WriteTimes.write(times, "TimeResultsStaticSecond.csv");
     }
 }
